9524e0093eb365b0a33a18a09a094ab306c68a8e
supisara-n <supisara.ngamchaipisit@gmail.com>
2026-03-30 17:41:12 +0700

    feat: separate read status for admin and researcher

--- internal/models/appointment.go
+++ internal/models/appointment.go
@@ -3,18 +3,19 @@ package models
 import "time"
 
 type Appointments struct {
-	ID        string    `gorm:"primaryKey;column:id" json:"id"`
-	CaseID    string    `gorm:"column:case_id;not null" json:"case_id"`
-	Case      *Cases    `gorm:"foreignKey:CaseID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"case"`
-	Date      time.Time `gorm:"column:date;not null" json:"date"`
-	Status    string    `gorm:"column:status;not null" json:"status"`
-	Location  string    `gorm:"column:location" json:"location"`
-	Detail    string    `gorm:"column:detail" json:"detail"`
-	Summary   string    `gorm:"column:summary" json:"summary"`
-	IsNotify  bool      `gorm:"column:is_notify" json:"is_notify"`
-	IsRead    bool      `gorm:"column:is_read;default:false" json:"is_read"`
-	CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
+	ID               string    `gorm:"primaryKey;column:id" json:"id"`
+	CaseID           string    `gorm:"column:case_id;not null" json:"case_id"`
+	Case             *Cases    `gorm:"foreignKey:CaseID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"case"`
+	Date             time.Time `gorm:"column:date;not null" json:"date"`
+	Status           string    `gorm:"column:status;not null" json:"status"`
+	Location         string    `gorm:"column:location" json:"location"`
+	Detail           string    `gorm:"column:detail" json:"detail"`
+	Summary          string    `gorm:"column:summary" json:"summary"`
+	IsNotify         bool      `gorm:"column:is_notify" json:"is_notify"`
+	IsReadAdmin      bool      `gorm:"column:is_read_admin;default:false" json:"is_read_admin"`
+	IsReadResearcher bool      `gorm:"column:is_read_researcher;default:false" json:"is_read_researcher"`
+	CreatedAt        time.Time `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt        time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
 
 func (Appointments) TableName() string {
